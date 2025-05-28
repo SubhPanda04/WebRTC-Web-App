a6f536cfbc3f9e212ee024d09e944560d6ef9c2b
Subhasis Panda <subhasispanda1256700@gmail.com>
2025-05-29 02:41:40 +0530

    resolved cors error

--- client-app/vite.config.js
+++ client-app/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config()
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [process.env.VITE_ALLOWED_HOST],
+  }
 })
